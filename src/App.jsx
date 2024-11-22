439b99ff9ffdc88c7810e3a9a3f67c44d6292b47
Mark Sopcak <mdsopcak@protonmail.com>
2024-11-22 13:47:45 -0500

    Created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/Header";
 
 function App() {
   return (
